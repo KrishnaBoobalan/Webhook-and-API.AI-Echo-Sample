7877161077df18c06ee5a02251be105aea4222e8
KrishnaBoobalan <45415782+KrishnaBoobalan@users.noreply.github.com>
2018-12-10 18:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,26 @@ restService.use(
 
 restService.use(bodyParser.json());
 
+var Operate = {
+  'Add': function(a,b){ return a + b },
+  'Sub': function(a,b){ return a - b },
+  'Mul': function(a,b){ return a * b },
+  'Div': function(a,b){ return a / b },
+};
+
+function gh(Op,a,b){
+  var Opr = Operate(op);
+  var Result = Calc(Math1,Math2);
+  Return
+}
+
 restService.post("/Math", function(req, res) {
   var Math1 = req.body.queryResult.parameters.Mathvalue1;
   var Math2 = req.body.queryResult.parameters.Mathvalue2;
-  var opr = req.body.queryResult.parameters.MathOperand;
-  var spech = "Successful";
+  var operator = req.body.queryResult.parameters.MathOperand;
+  Calc = Operate[operator];
+  var Result = Calc(Math1,Math2);
+  var spech = "Result is " + Result;
   return res.json({
   "fulfillmentText": spech,
   "payload": {
